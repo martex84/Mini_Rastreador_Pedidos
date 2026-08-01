46e0d64dd658287afec20a6b1e3798be5227e6ef
Marcelo Teixeira J <71203708+martex84@users.noreply.github.com>
2026-08-01 01:58:57 -0300

    chore(backend): inicializão da estrutura backend do projeto

---
+++ backend/mini-rastreador-pedidos/src/main/java/com/martex/mini_rastreador_pedidos/MiniRastreadorPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.martex.mini_rastreador_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRastreadorPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRastreadorPedidosApplication.class, args);
+	}
+
+}
---
+++ backend/mini-rastreador-pedidos/src/test/java/com/martex/mini_rastreador_pedidos/MiniRastreadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martex.mini_rastreador_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRastreadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
